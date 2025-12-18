4c62c8b1379d93d3247003c8faf9b19c40218c00
Eric Cornelissen <ericornelissen@gmail.com>
2025-12-18 21:36:34 +0100

    Add CommonJS support

---
+++ index.cjs
@@ -0,0 +1,16 @@
+const isSupportedRegexpFlag = require("is-supported-regexp-flag");
+
+const RegExp_ = globalThis.RegExp;
+
+let lRegExp;
+if (isSupportedRegexpFlag("l")) {
+	lRegExp = function(pattern, flags="") {
+		return new RegExp_(pattern, `${flags}l`);
+	};
+} else {
+	lRegExp = function(pattern, flags) {
+		return new RegExp_(pattern, flags);
+	};
+}
+
+module.exports = lRegExp;
