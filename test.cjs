dcd4791ac1312d07bd9d0200bb9eae50229928c7
Eric Cornelissen <ericornelissen@gmail.com>
2025-12-26 17:12:12 +0100

    Import `perf_hooks` instead of `node:perf_hooks`
    ... because otherwise on Node.js v15 we get "Error: Cannot find module
    'node:perf_hooks'"

--- test.cjs
+++ test.cjs
@@ -50,7 +50,7 @@
 /* -------------------------------------------------------------------------- */
 
 function time(cb) {
-	const { performance } = require("node:perf_hooks");
+	const { performance } = require("perf_hooks");
 
 	const start = performance.now();
 	cb();
