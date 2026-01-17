491df69bfa2305aca962fc09d636ebdc2bd5b70d
Eric Cornelissen <ericornelissen@gmail.com>
2026-01-18 00:24:15 +0100

    Handle non-string patterns in `lRegExp` constructor

--- index.cjs
+++ index.cjs
@@ -3,8 +3,15 @@ const isSupportedRegexpFlag = require("./is-supported-regexp-flag.cjs");
 let lRegExp = RegExp;
 
 if (isSupportedRegexpFlag("l")) {
-	lRegExp = function(pattern, flags="") {
-		return new RegExp(pattern, `${flags}l`);
+	lRegExp = function(pattern, flags) {
+	  if (flags === undefined) {
+      flags = "";
+      if (pattern instanceof RegExp && pattern.flags) {
+        flags = pattern.flags.replace(/l/g, "");
+      }
+    }
+
+    return new RegExp(pattern, `${flags}l`);
 	};
 }
 
--- index.js
+++ index.js
@@ -3,8 +3,15 @@ import isSupportedRegexpFlag from "is-supported-regexp-flag";
 let lRegExp = RegExp;
 
 if (isSupportedRegexpFlag("l")) {
-	lRegExp = function(pattern, flags="") {
-		return new RegExp(pattern, `${flags}l`);
+	lRegExp = function(pattern, flags) {
+    if (flags === undefined) {
+      flags = "";
+      if (pattern instanceof RegExp && pattern.flags) {
+        flags = pattern.flags.replace(/l/g, "");
+      }
+    }
+
+    return new RegExp(pattern, `${flags}l`);
 	};
 }
 
