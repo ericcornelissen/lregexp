79035d616cce3b29ef470451bf6c881516af81fe
Eric Cornelissen <ericornelissen@gmail.com>
2026-01-18 00:14:09 +0100

    Remove unnecessary locally scoped binding of global `RegExp`

--- index.cjs
+++ index.cjs
@@ -3,9 +3,8 @@ const isSupportedRegexpFlag = require("./is-supported-regexp-flag.cjs");
 let lRegExp = RegExp;
 
 if (isSupportedRegexpFlag("l")) {
-	const RegExp_ = globalThis.RegExp;
 	lRegExp = function(pattern, flags="") {
-		return new RegExp_(pattern, `${flags}l`);
+		return new RegExp(pattern, `${flags}l`);
 	};
 }
 
