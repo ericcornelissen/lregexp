09cd8d314e7baadea89cacc0b840d6ae942ad61d
Eric Cornelissen <ericornelissen@gmail.com>
2026-01-18 00:21:49 +0100

    Remove unnecessary locally scoped binding of global `RegExp` (#28)

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import isSupportedRegexpFlag from "is-supported-regexp-flag";
 let lRegExp = RegExp;
 
 if (isSupportedRegexpFlag("l")) {
-	const RegExp_ = globalThis.RegExp;
 	lRegExp = function(pattern, flags="") {
-		return new RegExp_(pattern, `${flags}l`);
+		return new RegExp(pattern, `${flags}l`);
 	};
 }
 
