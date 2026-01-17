8604cd70d0776d4ea3137ac36fd63eef2cdb01f4
Eric Cornelissen <ericornelissen@gmail.com>
2026-01-18 00:43:52 +0100

    Handle different runtime types for `pattern` argument (#27)

--- index.cjs
+++ index.cjs
@@ -3,7 +3,14 @@ const isSupportedRegexpFlag = require("./is-supported-regexp-flag.cjs");
 let lRegExp = RegExp;
 
 if (isSupportedRegexpFlag("l")) {
-	lRegExp = function(pattern, flags="") {
+	lRegExp = function(pattern, flags) {
+		if (flags === undefined) {
+			flags = "";
+			if (pattern instanceof RegExp && pattern.flags) {
+				flags = pattern.flags.replace(/l/g, "");
+			}
+		}
+
 		return new RegExp(pattern, `${flags}l`);
 	};
 }
--- index.js
+++ index.js
@@ -3,7 +3,14 @@ import isSupportedRegexpFlag from "is-supported-regexp-flag";
 let lRegExp = RegExp;
 
 if (isSupportedRegexpFlag("l")) {
-	lRegExp = function(pattern, flags="") {
+	lRegExp = function(pattern, flags) {
+		if (flags === undefined) {
+			flags = "";
+			if (pattern instanceof RegExp && pattern.flags) {
+				flags = pattern.flags.replace(/l/g, "");
+			}
+		}
+
 		return new RegExp(pattern, `${flags}l`);
 	};
 }
