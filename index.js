e9a72e97687e3d604be949f117f786b00882e8a4
Eric Cornelissen <ericornelissen@gmail.com>
2026-01-18 21:29:05 +0100

    Add description for return value

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import isSupportedRegexpFlag from "is-supported-regexp-flag";
  *
  * @param {string|RegExp} pattern The text of the regular expression. This can also be another RegExp object.
  * @param {string} [flags] If specified, flags is a string that contains the flags to add. Alternatively, if a RegExp object is supplied for the pattern, the flags string will replace any of that object's flags.
- * @returns {RegExp}
+ * @returns {RegExp} A {@link RegExp} object constructed from `pattern` and `flags`.
  * @throws {SyntaxError} Thrown in one of the following cases: `pattern` cannot be parsed as a valid regular expression, or `flags` contains repeated characters or any character outside of those allowed.
  */
 let lRegExp = RegExp;
